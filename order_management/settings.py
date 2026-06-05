4eceb7040190a6de154b0b96d7ec735011b4ccd9
Reza <r.razifar1999@gmail.com>
2026-06-05 22:20:19 +0330

    docker-compose has been fixed.

--- order_management/settings.py
+++ order_management/settings.py
@@ -20,6 +20,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
